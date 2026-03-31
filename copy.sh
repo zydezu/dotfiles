223240a9e5aa985b6e574747e11a98b7da96a1ba
zydezu <kinomaxxing@gmail.com>
2026-03-31 17:37:59 +0100

    FIX: update dotfiles

--- copy.sh
+++ copy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd ~/.config
 shopt -s dotglob
+
+allowed_dirs=(actions-for-nautilus alacritty environment.d fastfetch fish gpu-screen-recorder mango matugen mprisence mpv rofi swaync xdg-desktop-portal zed)
+
 for dir in */; do
-    cp -r "$dir" /home/zy/Documents/projects/coding/dotfiles/
+    for allowed in "${allowed_dirs[@]}"; do
+        if [[ "$dir" == "$allowed/" ]]; then
+            cp -r "$dir" /home/zy/Documents/projects/coding/dotfiles/
+            break
+        fi
+    done
 done
