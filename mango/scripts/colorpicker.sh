97e71d19123cc6c84589a52b0ff4eb2df74b9ccc
zydezu <kinomaxxing@gmail.com>
2026-05-19 21:14:50 +0100

    FIX: improve colorpicker.sh, showing a notification, and fixing \n bug

--- mango/scripts/colorpicker.sh
+++ mango/scripts/colorpicker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 grim -g "$(slurp -p)" -t ppm - | magick - -format '%[pixel:u]' info:- | \
   sed 's/srgb(\([0-9]*\),\([0-9]*\),\([0-9]*\))/\1 \2 \3/' | \
-  awk '{printf "#%02X%02X%02X\n", $1, $2, $3}' | \
-  tee >(wl-copy)
+  awk '{printf "#%02X%02X%02X", $1, $2, $3}' | \
+  tee >(wl-copy) | \
+  dunstify "Colour hex value copied to clipboard" "$(cat)" -t 2000
